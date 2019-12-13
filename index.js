cd7a200e84ed3169bd7376eac2811b9a8a5cb5ad
Yacine Mohamdi <33285087+ycnmhd@users.noreply.github.com>
2019-12-13 18:02:58 +0100

    add an option to skip underscore

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var postcss = require('postcss');
 
-var pattern = /[-_.]+([a-zA-Z0-9])/g;
-
-function replacer (g) {
-
+function replacer(g) {
     var character = g[g.length - 1];
 
     return g[0] === '.' ? '.' + character.toLowerCase() : character.toUpperCase();
 }
 
-module.exports = postcss.plugin('postcss-camel-case', function() {
-
+module.exports = postcss.plugin('postcss-camel-case', function (opts = {}) {
+    var pattern = new RegExp(
+        `[-.${opts.skipUnderscore ? '' : '_'}]+([a-zA-Z])`,
+        'g'
+    );
     return function (css) {
-
         css.walkRules(function (rule) {
-
             rule.selector = rule.selector.replace(pattern, replacer);
         });
     };
